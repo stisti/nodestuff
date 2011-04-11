f48c80a82a60c89897b233a673d07cf58d60b0d0
Sami Tikka <sjtikka@gmail.com>
2011-04-12 00:34:39 +0300

    Control the number of files processed so process won't run out of fds.

--- sha256.js
+++ sha256.js
@@ -7,27 +7,28 @@
 var crypto = require('crypto');
 var fs = require('fs');
 
-// Find the streams to read from command line parameters
-var streams = [];
-process.argv.slice(2).forEach(function(file, index, array) {
-    streams.push({ 'file':file, 'stream':fs.createReadStream(file) });
-});
+var files = process.argv.slice(2);
 
-// If no files, read stdin
-if (streams.length == 0) {
-    streams.push({ 'file':'stdin', 'stream':process.stdin });
-    process.stdin.resume();
-}
-
-// Do it all in parallel
-streams.forEach(function(val, index, array) {
-    var file = val['file'];
-    var stream = val['stream'];
+function startHashing(stream) {
     var h = crypto.createHash('sha256');
     stream.on('data', function(chunk) {
 	h.update(chunk);
     });
     stream.on('end', function() {
-	process.stdout.write('SHA256 (' + file + ") = " + h.digest('hex') + '\n');
+	process.stdout.write('SHA256 (' + stream.path + ") = " + h.digest('hex') + '\n');
+	if (files.length > 0) {
+	    startHashing(fs.createReadStream(files.shift()));
+	}
     });
-});
+}
+
+// If no files, read stdin
+if (files.length == 0) {
+    process.stdin.resume();
+    startHashing(process.stdin);
+} else {
+    files.splice(0, 100).forEach(function(f) {
+	startHashing(fs.createReadStream(f));
+    });
+}
+
