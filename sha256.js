0466267f0053018107e4b8e60a1caa291d2ad740
Sami Tikka <sjtikka@gmail.com>
2011-05-01 20:10:43 +0300

    Revert "Rewrote using low-level fs API to improve performance. Failed."
    This reverts commit 8993223876b518ab76cdf08b28c46d5b43126f36.
    The use of low-level API made the script longer and harder to
    understand but it didn't make it any faster. Node is still spending
    most of its time in UTF-8 string allocation.

--- sha256.js
+++ sha256.js
@@ -10,46 +10,24 @@ var fs = require('fs');
 var files = process.argv.slice(2);
 
 var algorithm = 'sha256';
-var bufSize = 16*1024;
 
-function startReading(fileName, fd, hash, buf) {
-    //console.log('Starting to read ' + fileName);
-    fs.read(fd, buf, 0, bufSize, null, 
-	    function (err, bytesRead, buf) {
-		if (err) {
-		    console.log(err);
-		    fs.close(fd);
-		    if (files.length > 0) {
-			startHashing(files.shift());
-		    }
-		} else {
-		    if (bytesRead > 0) {
-			//console.log('Got ' + bytesRead + ' bytes');
-			hash.update(buf.slice(0, bytesRead));
-			startReading(fileName, fd, hash, buf);
-		    } else if (bytesRead == 0) {
-			process.stdout.write(algorithm.toUpperCase() + 
-					     ' (' + fileName + ") = " + 
-					     hash.digest('hex') + '\n');
-			fs.close(fd);
-			if (files.length > 0) {
-			    startHashing(files.shift());
-			}
-		    }
-		}
-	    });
-}
-
-function startHashing(fileName) {
-    //console.log('Opening ' + fileName);
-    fs.open(fileName, 'r', 0666, function(err, fd) {
-	if (err) {
-	    console.log(err);
-	    if (files.length > 0) {
-		startHashing(files.shift());
-	    }
-	} else {
-	    startReading(fileName, fd, crypto.createHash(algorithm), new Buffer(bufSize));
+function startHashing(stream) {
+    var h = crypto.createHash(algorithm);
+    stream.on('data', function(chunk) {
+	h.update(chunk);
+    });
+    stream.on('end', function() {
+	process.stdout.write(algorithm.toUpperCase() + 
+			     ' (' + stream.path + ") = " + 
+			     h.digest('hex') + '\n');
+	if (files.length > 0) {
+	    startHashing(fs.createReadStream(files.shift()));
+	}
+    });
+    stream.on('error', function(exception) {
+	console.log('' + exception);
+	if (files.length > 0) {
+	    startHashing(fs.createReadStream(files.shift()));
 	}
     });
 }
@@ -59,8 +37,8 @@ if (files.length == 0) {
     process.stdin.resume();
     startHashing(process.stdin);
 } else {
-    files.splice(0, 10).forEach(function(f) {
-	startHashing(f);
+    files.splice(0, 100).forEach(function(f) {
+	startHashing(fs.createReadStream(f));
     });
 }
 
