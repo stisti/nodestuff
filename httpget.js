737e1a42b52a68fb78d70dc88bff16d426252fca
Sami Tikka <sjtikka@gmail.com>
2012-01-16 23:46:57 +0200

    JS version of httpget

---
+++ httpget.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+var program = require('commander');
+program
+    .version('0.0.1')
+    .option('-r, --reload', 'reload the URL when talking to a caching proxy')
+    .parse(process.argv);
+
+var http = require('http');
+var url = require('url');
+
+function http_get(urls) {
+    if (!urls) {
+        return;
+    }
+    var firstUrl = urls.shift();
+    if (!firstUrl) {
+        return;
+    }
+    var parse = url.parse(firstUrl);
+    var req = http.request(
+        {
+            method: 'GET',
+            host:   parse.host,
+            port:   parse.port ? parse.port : 80,
+            path:   parse.path
+        }, 
+        function (res) {
+            process.stderr.write(res.statusCode + '\n');
+            for (var h in res.headers) {
+                process.stderr.write(h + ': ' + res.headers[h] + '\n');
+            }
+            res.pipe(process.stdout, { end: false });
+            res.on('end', function () {
+                http_get(urls);
+            });
+            res.on('error', function (e) {
+                console.error(e.message);
+                http_get(urls);
+            });
+        });
+    req.on('error', function (e) {
+        console.error(e.message);
+        http_get(urls);
+    });
+    req.end();
+};
+
+http_get(program.args);
